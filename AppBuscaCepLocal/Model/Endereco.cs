4fdc99bc399c0a2c8e7483cfecbc1faa4b51a3d8
Usuario <Usuario@CPD03-M02>
2024-09-23 12:10:00 -0300

    novas alterações

--- AppBuscaCepLocal/Model/Endereco.cs
+++ AppBuscaCepLocal/Model/Endereco.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppBuscaCepLocal.Model
+﻿namespace AppBuscaCepLocal.Model
 {
-    internal class Endereco
+    public class Endereco
     {
-
+        public int id_logradouro { get; set; }
+        public int id_cidade { get; set; }
+        public string tipo { get; set; }
+        public string descricao { get; set; }
+        public string uf { get; set; }
+        public string complemento { get; set; }
+        public string descricao_sem_numero { get; set; }
+        public object rows { get; set; }
+        public int CEP { get; set; }
+        public string UF { get; set; }
     }
 }
